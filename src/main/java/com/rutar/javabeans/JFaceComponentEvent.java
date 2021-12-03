131627c316f4d4e90663931bcc828a025f05c215
RutarAndriy <RutarAndriy@gmail.com>
2021-12-03 23:08:45 +0200

    Added JFaceComponentEvent.java file

---
+++ src/main/java/com/rutar/javabeans/JFaceComponentEvent.java
@@ -0,0 +1,46 @@
+package com.rutar.javabeans;
+
+import java.awt.*;
+
+// ............................................................................
+
+public class JFaceComponentEvent extends AWTEvent {
+
+private final Object oldValue;
+private final Object newValue;
+
+public static final int JFACECOMPONENT_EVENT = AWTEvent.RESERVED_ID_MAX + 333;
+
+///////////////////////////////////////////////////////////////////////////////
+
+public JFaceComponentEvent (Object source,
+                            Object oldValue, Object newValue) {
+
+super(source, JFACECOMPONENT_EVENT);
+
+this.oldValue = oldValue;
+this.newValue = newValue;
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+public Object getOldValue() { return oldValue; }
+public Object getNewValue() { return newValue; }
+
+///////////////////////////////////////////////////////////////////////////////
+
+@Override
+public String toString() {
+
+    return getClass().getName() +
+            
+        "[" +
+            "oldValue=" + oldValue + "; " +
+            "newValue=" + newValue +
+        "]";
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+}
